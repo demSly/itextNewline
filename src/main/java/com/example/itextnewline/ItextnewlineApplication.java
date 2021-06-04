ef24953ed515f67aa2a0b15c25218771f1ca071b
Marco Dell'Anna <dem@sly.ch>
2021-06-04 13:32:09 +0200

    Initial commit

---
+++ src/main/java/com/example/itextnewline/ItextnewlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.itextnewline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItextnewlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItextnewlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itextnewline/ItextnewlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itextnewline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItextnewlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
